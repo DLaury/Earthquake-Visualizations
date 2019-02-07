d32cb6798c49f1428109c547270fd5871ba08b4b
Dan Laury <DLaury09@gmail.com>
2019-02-06 18:20:55 -0600

    Show map and figure base settings

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,14 @@
-// Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.light",
-  accessToken: config.MY_KEY
-});
+// Create map object
+var usMap = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 4
+  });
+
+// Add a tile layer (the background map image) to our map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: config.MY_KEY
+  }).addTo(usMap);
+  
